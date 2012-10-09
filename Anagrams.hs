0f736b929263cb3e7fb83703c99c0ca60093bbea
Markus Hauck <markus1189@gmail.com>
2012-10-09 18:51:17 +0200

    Eliminate sorting by using Sets
    Instead of sorting a String and storing it's hash in the map,
    create a set of the chars of the target string and store the
    set in the map

--- Anagrams.hs
+++ Anagrams.hs
@@ -1,14 +1,13 @@
 import qualified Data.Map as M
-
-import Data.List (sort)
+import qualified Data.Set as S
 
 wordlist :: IO [String]
 wordlist = fmap lines $ readFile "wordlist.txt"
 
-type AnagramMap = M.Map String [String]
+type AnagramMap = M.Map (S.Set Char) [String]
 
 insert :: AnagramMap -> String -> AnagramMap
-insert mp str = M.insertWith (++) (sort str) [str] mp
+insert mp str = M.insertWith (++) (S.fromList str) [str] mp
 
 buildMap :: [String] -> AnagramMap
 buildMap = foldl insert M.empty
